69b3ddb6f899b8cdff17a9cf10fe834ca339785e
Yuan Zi <ziy20@mails.tsinghua.edu.cn>
2023-02-26 08:54:20 +0800

    add insert non-aligned column records

--- client_v2/session.go
+++ client_v2/session.go
@@ -124,6 +124,19 @@ func (c *Client) InsertColumnRecords(ctx context.Context, req *rpc.InsertColumnR
 	return nil
 }
 
+func (c *Client) InsertNonAlignedColumnRecords(ctx context.Context, req *rpc.InsertNonAlignedColumnRecordsReq) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	status, err := c.client.InsertNonAlignedColumnRecords(ctx, req)
+	if err != nil {
+		return errors.Wrap(ErrNetwork, err.Error())
+	}
+	if err = verifyStatus(status); err != nil {
+		return err
+	}
+	return nil
+}
+
 func NewSession(settings *SessionSettings) *Session {
 	return &Session{
 		settings:     settings,
@@ -510,6 +523,121 @@ func (s *Session) InsertColumnRecords(paths []string, timestamps []int64, valueL
 		if reconnect {
 			req.SessionId = s.sessionId
 			err = s.client.InsertColumnRecords(context.Background(), &req)
+		}
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Session) InsertNonAlignedColumnRecords(paths []string, timestamps []int64, valueList [][]interface{}, dataTypeList []rpc.DataType, tagsList []map[string]string) error {
+	if paths == nil || timestamps == nil || valueList == nil || dataTypeList == nil ||
+		len(paths) == 0 || len(timestamps) == 0 || len(valueList) == 0 || len(dataTypeList) == 0 {
+		return errors.New("invalid insert request")
+	}
+	if len(paths) != len(dataTypeList) {
+		return errors.New("the sizes of paths and dataTypeList should be equal")
+	}
+	if tagsList != nil && len(paths) != len(tagsList) {
+		return errors.New("the sizes of paths and tagsList should be equal")
+	}
+	if len(paths) != len(valueList) {
+		return errors.New("the sizes of paths and valuesList should be equal")
+	}
+
+	// 保证时间戳递增
+	timeIndex := make([]int, len(timestamps))
+	for i := range timestamps {
+		timeIndex[i] = i
+	}
+	sort.Slice(timeIndex, func(i, j int) bool {
+		return timestamps[i] < timestamps[j]
+	})
+	sort.Slice(timestamps, func(i, j int) bool {
+		return timestamps[i] < timestamps[j]
+	})
+	for i := range valueList {
+		var values []interface{}
+		for j := range timestamps {
+			values = append(values, valueList[i][timeIndex[j]])
+		}
+		valueList[i] = values
+	}
+	// 保证序列递增
+	pathIndex := make([]int, len(paths))
+	for i := range paths {
+		pathIndex[i] = i
+	}
+	sort.Slice(pathIndex, func(i, j int) bool {
+		return paths[i] < paths[j]
+	})
+	sort.Strings(paths)
+	// 重排数据和数据类型
+	var sortedValueList [][]interface{}
+	var sortedDataTypeList []rpc.DataType
+	for i := range pathIndex {
+		sortedValueList = append(sortedValueList, valueList[pathIndex[i]])
+		sortedDataTypeList = append(sortedDataTypeList, dataTypeList[pathIndex[i]])
+	}
+	// 重排tagKV
+	var sortedTagsList []map[string]string
+	if tagsList != nil {
+		for i := range pathIndex {
+			sortedTagsList = append(sortedTagsList, tagsList[pathIndex[i]])
+		}
+	}
+	// 压缩数据
+	var valueBufferList, bitmapBufferList [][]byte
+	for i := range sortedValueList {
+		rowValues := sortedValueList[i]
+		rowValuesBuffer, err := ColumnValuesToBytes(rowValues, sortedDataTypeList[i])
+		if err != nil {
+			return err
+		}
+		valueBufferList = append(valueBufferList, rowValuesBuffer)
+		bitmap := NewBitmap(len(rowValues))
+		for j := range rowValues {
+			if rowValues[j] != nil {
+				err = bitmap.Mark(j)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		bitmapBufferList = append(bitmapBufferList, bitmap.GetBitmap())
+	}
+	timeBytes, err := TimestampsToBytes(timestamps)
+	if err != nil {
+		return err
+	}
+
+	req := rpc.InsertColumnRecordsReq{
+		SessionId:    s.sessionId,
+		Paths:        paths,
+		Timestamps:   timeBytes,
+		ValuesList:   valueBufferList,
+		BitmapList:   bitmapBufferList,
+		DataTypeList: sortedDataTypeList,
+		TagsList:     sortedTagsList,
+	}
+
+	err = s.client.InsertColumnRecords(context.Background(), &req)
+	if errors.Cause(err) == ErrExecution {
+		return err
+	}
+	if errors.Cause(err) == ErrNetwork {
+		reconnect := s.reconnect()
+		if !reconnect && s.settings.EnableHighAvailable {
+			if err := s.Open(); err == nil {
+				reconnect = true
+			} else {
+				log.Println("switch to other IGinX failure: " + err.Error())
+			}
+		}
+		if reconnect {
+			req.SessionId = s.sessionId
+			err = s.client.InsertNonAlignedColumnRecords(context.Background(), &req)
 		}
 	}
 	if err != nil {
